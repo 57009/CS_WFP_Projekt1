08e3cd2b2d75d0e3a622b5381d0a85a5810afb9c
Tomasz Chorzępa <w57009@student.wsiz.rzeszow.pl>
2018-10-12 17:06:21 +0200

    3 randomization methods implemented

--- Generator/Generator/MainWindow.xaml.cs
+++ Generator/Generator/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MaterialDesignThemes;
 using MaterialDesignColors;
+using System.Security.Cryptography;
+
 namespace Generator
 {
     /// <summary>
@@ -29,7 +31,6 @@ namespace Generator
         private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-
         }
 
         private void GridBarTop_MouseDown(object sender, MouseButtonEventArgs e)
@@ -48,7 +49,91 @@ namespace Generator
         private void CleanListButton_Click(object sender, RoutedEventArgs e)
         {
             this.OutputTextBox.Text = null;
+        }
+
+        //https://cezarywalenciuk.pl/blog/programing/post/c-string-z-losowymi-znaki
+
+
+        public static string RandomString(int range) // LINQ “Range” - method 1
+        {
+            var chars = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, range)
+                            .Select(s => s[random.Next(s.Length)])
+                            .ToArray());
+
+            return result;
+        }
+
+
+        public static string GetUniqueKey(int maxSize) // RNGCryptoServiceProvider - method 2
+        {
+            char[] chars = new char[62];
+            chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetBytes(data);
+            data = new byte[maxSize];
+            crypto.GetBytes(data);
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+        public static string GuidString(int numOfCharsNeeded) // GUID - method 3
+        {
+            if (numOfCharsNeeded <= 32)
+                return Guid.NewGuid().ToString("n").Substring(0, numOfCharsNeeded);
+            else
+            {
+                int f = numOfCharsNeeded / 32 + 1;
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i <= f; i++)
+                {
+                    sb.Append(Guid.NewGuid().ToString("n"));
+                }
+
+                return sb.ToString().Remove(numOfCharsNeeded);
+            }
+        }
+
+
+        public string PickMethod(int numOfCharsGen)
+        {
+           
+            var z1 = m1RadioButton.IsChecked;
+            var z2 = m2RadioButton.IsChecked;
+            var z3 = m3RadioButton.IsChecked;
+            string returnString ="";
+            if ( z1 == true)
+            {
+                returnString = RandomString(numOfCharsGen);
+            }
+            else  if (z2 == true)
+            {
+                returnString = GetUniqueKey(numOfCharsGen);
+            }
+            else   if (z3 == true)
+            {
+                returnString = GuidString(numOfCharsGen);
+            }
+            
+            return returnString;
 
         }
+
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.OutputTextBox.Text += PickMethod(4)+"\n";
+
+        }
+
     }
 }
