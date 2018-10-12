222a6e76f01f8a427f8805e9f96767d4c1e42f43
Tomasz Chorzępa <w57009@student.wsiz.rzeszow.pl>
2018-10-12 19:12:54 +0200

    Patterns and rand methods implemented

--- Generator/Generator/MainWindow.xaml.cs
+++ Generator/Generator/MainWindow.xaml.cs
@@ -15,6 +15,11 @@ using System.Windows.Shapes;
 using MaterialDesignThemes;
 using MaterialDesignColors;
 using System.Security.Cryptography;
+using System.Threading;
+
+//
+
+using System.Data;
 
 namespace Generator
 {
@@ -67,11 +72,29 @@ namespace Generator
         }
 
 
-        public static string GetUniqueKey(int maxSize) // RNGCryptoServiceProvider - method 2
+        public static string GetUniqueKey(int maxSize) // RNGCryptoServiceProvider - method 2 (Secure Random Numbers)
         {
             char[] chars = new char[62];
             chars =
             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[1];
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetBytes(data);
+            data = new byte[maxSize];
+            crypto.GetBytes(data);
+            StringBuilder result = new StringBuilder(maxSize);
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length)]);
+            }
+            return result.ToString();
+        }
+
+        public static string GetUniqueKeyDigits(int maxSize) // RNGCryptoServiceProvider - method 2.1 digits only
+        {
+            char[] chars = new char[62];
+            chars =
+            "1234567890".ToCharArray();
             byte[] data = new byte[1];
             RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
             crypto.GetBytes(data);
@@ -110,10 +133,12 @@ namespace Generator
             var z1 = m1RadioButton.IsChecked;
             var z2 = m2RadioButton.IsChecked;
             var z3 = m3RadioButton.IsChecked;
+            var z4 = m4RadioButton.IsChecked;
             string returnString ="";
             if ( z1 == true)
             {
                 returnString = RandomString(numOfCharsGen);
+                Thread.Sleep(30);
             }
             else  if (z2 == true)
             {
@@ -123,16 +148,118 @@ namespace Generator
             {
                 returnString = GuidString(numOfCharsGen);
             }
-            
+            else if (z4 == true)
+            {
+                returnString = GetUniqueKeyDigits(numOfCharsGen);
+            }
+
             return returnString;
 
         }
 
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
-        {
-            this.OutputTextBox.Text += PickMethod(4)+"\n";
 
+
+
+        private void ButtonGen1_Click(object sender, RoutedEventArgs e)
+        {
+
+            //  int a = int.Parse(textBoxAmount.Text);
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(4) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.","Perse error");
+            }
+
+              
+        }
+
+        private void ButtonGen2_Click(object sender, RoutedEventArgs e)
+        {
+            
+                
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(6) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.", "Perse error");
+            }
+        }
+
+        private void ButtonGen3_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(4) + "-" + PickMethod(4) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.", "Perse error");
+            }
+        }
+
+        private void ButtonGen4_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(3) + "-" + PickMethod(3) + "-" + PickMethod(3) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.", "Perse error");
+            }
+        }
+
+        private void ButtonGen5_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(4) + "-" + PickMethod(4) + "-" + PickMethod(4) + "-" + PickMethod(2) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.", "Perse error");
+            }
+        }
+
+        private void ButtonGen6_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.TryParse(textBoxAmount.Text, out int a))
+            {
+
+                for (int i = 0; i < a; i++)
+                {
+                    this.OutputTextBox.Text += PickMethod(4) + "-" + PickMethod(4) + "-" + PickMethod(4) + "-" + PickMethod(4) + "\n";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Type integer value in amount section.", "Perse error");
+            }
         }
 
     }
